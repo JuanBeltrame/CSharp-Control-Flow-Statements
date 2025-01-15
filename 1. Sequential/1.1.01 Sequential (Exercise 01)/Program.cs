55eb4097e7867708d48fd6b7c63394135fcf47e3
Juan Beltrame <juan_beltrame@hotmail.com>
2025-01-15 04:11:20 -0300

    New excersice added

--- 1. Sequential/1.1.01 Sequential (Exercise 01)/Program.cs
+++ 1. Sequential/1.1.01 Sequential (Exercise 01)/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace _1._1._01_Sequence
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿/*
+Practica 1 - Sequential - Exercise 1.1.01
+----------------------------------------------------------------------
+English:
+Given two numbers entered via keyboard as data, 
+calculate their sum and report the result. 
+Declare the variables to be used: N1, N2, SUM, all of numerical type.
+
+Spanish: 
+Dados como datos (ingresados por teclado) dos números cualesquiera 
+calcular su suma e informar el resultado.
+Declarar las variables a utilizar N1, N2, SUMA de tipo numérico
+----------------------------------------------------------------------
+*/
+
+static int CalculteSum(int firstValue, int secondValue) => firstValue + secondValue;
+
+int firstValue = default;
+int secondValue = default;
+
+
+firstValue = int.Parse(Console.ReadLine()!);
+secondValue = int.Parse(Console.ReadLine()!);
+int result = CalculteSum(firstValue, secondValue);
+Console.WriteLine(result);
+
+
+
