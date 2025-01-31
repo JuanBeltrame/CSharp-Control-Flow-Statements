06aaef5aae8f431629e26466f8e696e13eef9287
Juan Beltrame <juan_beltrame@hotmail.com>
2025-01-31 03:46:11 -0300

    refactor: restructure solution and add new exercises
    - Removed project "2.1.04 If Condition (Exercise 04)" from
      `Sequential.sln`.
    - Added new projects "2.1.04.1 If Condition (Exercise 04)" and
      "2.1.04.2 If Condition (Exercise 04)".
    - Created `2.1.04.1 If Condition (Exercise 04).csproj` targeting
      .NET 8.0 with standard SDK structure.
    - Added `Program.cs` for `2.1.04.1` with user prompts and
      ascending order checks in English and Spanish.
    - Created `2.1.04.2 If Condition (Exercise 04).csproj` targeting
      .NET 9.0 with similar properties.
    - Added `Program.cs` for `2.1.04.2` with enhanced input
      validation using `int.TryParse`.
    - Overall improvements enhance user experience and code
      robustness.

---
+++ 2. Selection statements/2.1.04.2 If Condition (Exercise 04)/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+Practica 1 - If Conditional - Exercise 2.1.04
+----------------------------------------------------------------------
+English:
+Enter three distinct integer numbers. 
+Determine and display whether they were entered in ascending order.
+
+Spanish: 
+Ingresar tres números enteros distintos. 
+Determinar y mostrar si ingresaron en orden creciente.
+----------------------------------------------------------------------
+*/
+
+Console.Write("Ingresar el PRIMER NUMERO: ");
+if (!int.TryParse(Console.ReadLine(), out int numberA))
+{
+    Console.WriteLine($"El pimer valor ingresado, no es un numero");
+    return;
+}
+
+Console.Write("Ingresar el SEGUNDO NUMERO: ");
+if (!int.TryParse(Console.ReadLine(), out int numberB))
+{
+    Console.WriteLine($"El segundo valor ingresado, no es un numero");
+    return;
+}
+
+Console.Write("Ingresar el TERCER NUMERO: ");
+if (!int.TryParse(Console.ReadLine(), out int numberC))
+{
+    Console.WriteLine($"El tercer valor ingresado, no es un numero");
+    return;
+}
+
+if (!(numberA != numberB) || !(numberA != numberC) || !(numberB != numberC))
+    Console.WriteLine("Existen numeros repetidos");
+
+if((numberA < numberB) || (numberB < numberC))
+    Console.WriteLine($"Los numeros se ingresaron en orden creciente {numberA}, {numberB}, {numberC}");
+else
+    Console.WriteLine($"Los numeros se ingresaron de manera desordenada {numberA}, {numberB}, {numberC}");
+
+
+
+
