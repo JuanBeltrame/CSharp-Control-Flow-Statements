c9db8424c70f98dc35092dd93addb7705fd088fc
Juan Beltrame <juan_beltrame@hotmail.com>
2025-02-08 03:48:32 -0300

    feat: add Exercise 05 project and implementation
    - Added a new project "2.1.05.2 If Condition (Exercise 05)" to the
      solution file `CSharp-Control-Flow-Statements.sln` with
      configuration for Debug and Release modes.
    
    - Created a new C# project file `2.1.05.2 If Condition
      (Exercise 05).csproj` specifying the project SDK, output type,
      target framework, root namespace, and enabling implicit usings
      and nullable reference types.
    
    - Implemented a new `Program.cs` file that prompts the user to
      enter three numbers, evaluates if the first number is less than
      the other two, and displays the results.

---
+++ 2. Selection statements/2.1.05.2 If Condition (Exercise 05)/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+Practica 1 - If Conditional - Exercise 2.1.05
+----------------------------------------------------------------------
+English:
+Determine if the first of a set of three given numbers 
+is less than the other two.
+
+Spanish: 
+Determinar si el primero de un conjunto de tres números dados, 
+es menor que los otros dos.
+----------------------------------------------------------------------
+*/
+
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("Enter three numbers: ");
+Console.Write($"First: "); int firstNumber = int.Parse(Console.ReadLine()!);
+Console.Write($"Second: "); int secondNumber = int.Parse(Console.ReadLine()!);
+Console.Write("Third: "); int thirdNumber = int.Parse(Console.ReadLine()!);
+
+DisplayNumbers(firstNumber, secondNumber, thirdNumber);
+EvaluateValues(firstNumber, secondNumber, thirdNumber);
+
+static void EvaluateValues(int n1, int n2, int n3)
+{
+    if ((n1 < n2) && (n1 < n3))
+        Console.WriteLine("El primer numero ingresado es menor que los otros dos");
+    else
+        Console.WriteLine("El primer numero ingresado no es el menor de los tres");
+}
+static void DisplayNumbers(int n1, int n2, int n3)
+{
+    List<int> numberList = [n1, n2, n3];
+    Console.Write($"Numbers are: ");
+    foreach (int number in numberList)
+        Console.Write($"{number} ");
+
+    Console.WriteLine();
+}
