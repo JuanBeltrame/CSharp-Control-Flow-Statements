84f3a13f2d101bb9a1ca14fc84663205c5c5aabd
Juan Beltrame <juan_beltrame@hotmail.com>
2025-02-18 04:12:32 -0300

    feat: add measurement validation for type 'B' pieces
    - Implemented measurement validation for pieces of type 'B'
      with a valid range of 177 mm to 183 mm, allowing for a
      tolerance of +/- 3 mm.
    
    - Added error handling for invalid character input and
      non-integer measurements to ensure proper user input
      validation.

--- 2.1.07.2 If Condition (Exercise 07)/Program.cs
+++ 2.1.07.2 If Condition (Exercise 07)/Program.cs
@@ -21,3 +21,46 @@ d. Las piezas de tipo B deben medir 180 mm y se admite un error de +/-3 mm.
 
 Console.Title = "Practica 1 - If Conditional - Exercise 2.1.07";
 Console.ForegroundColor = ConsoleColor.Yellow;
+
+string? result = default;
+
+Console.Write("Indicar el Tipo de Pieza: A/B: ");
+if (!char.TryParse(Console.ReadLine()!, out char value))
+{
+    Console.WriteLine("Se debe ingresar un tipo de dato CHAR");
+    return;
+}
+Console.WriteLine();
+
+Console.Write("Indicar la medida de la pieza: ");
+if (!int.TryParse(Console.ReadLine()!, out int measurement))
+{
+    Console.WriteLine("La medida debe ser de Tipo Entero");
+    return;
+}
+
+// Pattern Matching: Constant Pattern
+if (value is 'A')
+{
+    // Logical/Relational Pattern 
+    result = measurement switch
+    {
+        >= 163 and <= 168 => "esta dentro del rango",
+        < 163 or > 168 => "esta fuera del rango"
+    };
+}
+else if (value is 'B')
+{
+    result = measurement switch
+    {
+        >= 177 and <= 183 => "esta dentro del rango",
+        < 177 or > 183 => "esta fuera del rango"
+    };
+}
+
+// Aca se utilizo raw interpolated string literals
+Console.WriteLine($"""
+    . Se Ingreso la pieza: {value}");
+    . La medida de la pieza es: {measurement}
+    . La pieza {value} {result}
+    """);
