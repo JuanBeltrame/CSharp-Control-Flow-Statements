0164dcf0f8c55b6c11486331d3c52a57f07e70ea
Juan Beltrame <juan_beltrame@hotmail.com>
2025-02-18 04:21:25 -0300

    feat: add Exercise 08 project and description
    - Added new project "2.1.08.2 If Condition (Exercise 08)" to
      `CSharp-Control-Flow-Statements.sln`.
    - Created project file `2.1.08.2 If Condition (Exercise 08).csproj`
      with .NET SDK, including output type, target framework, and
      nullable reference types.
    - Added comment block in `Program.cs` detailing the exercise
      requirements in English and Spanish.

---
+++ 2. Selection statements/2.1.08.2 If Condition (Exercise 08)/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Practica 1 - If Conditional - Exercise 2.1.08
+----------------------------------------------------------------------
+English:
+Three positive numbers are read. 
+Determine whether they are the lengths of the sides of a triangle. 
+Remember that in any triangle, each side is less than or equal to the sum of the other two sides and less than their difference (it is sufficient to show this for one side). 
+If so, report whether the triangle is:
+
+Equilateral (3 equal sides),
+Isosceles (2 equal sides), or
+Scalene (3 different sides)
+
+Spanish: 
+Se leen tres números positivos. 
+Determinar si son las longitudes de los lados de un triángulo: 
+Recordar que en todo triangulo cada lado es menor o igual que la suma de los
+otros dos y menor que su diferencia (basta mostrarlo para un lado). 
+En caso afirmativo, informar si el mismo es:
+
+equilátero (3 lados iguales), 
+isósceles (2 lados iguales) o
+escaleno (3 lados distintos).
+----------------------------------------------------------------------
+*/
